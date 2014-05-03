05b10f4f4a4b351662bbe6dfed7e3242af75f5b8
danilau <danilau@tut.by>
2014-05-02 19:29:34 -0700

    Initial Commit

---
+++ SmartPecker/SPAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAAppDelegate.h
+//  SmartPecker
+//
+//  Created by majstrak on 02.05.14.
+//  Copyright (c) 2014 Danilau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartPecker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartPecker
+//
+//  Created by majstrak on 02.05.14.
+//  Copyright (c) 2014 Danilau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAAppDelegate class]));
+    }
+}
