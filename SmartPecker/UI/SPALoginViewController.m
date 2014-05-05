84ee2659083daaf63fb32a3ca85e5101c75346b6
danilau <danilau@tut.by>
2014-05-04 22:05:03 -0700

    LoginScene: Add keyboard and constraints

--- SmartPecker/UI/SPALoginViewController.m
+++ SmartPecker/UI/SPALoginViewController.m
@@ -10,6 +10,16 @@
 
 @interface SPALoginViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *loginTextField;
+
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *logoVerticalSpaceConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *loginTextFieldVerticalSpaceConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *passwordTextFieldVerticalSpaceConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *forgetPasswordButtonVerticalSpaceConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *loginButtonVerticalSpaceConstraint;
+
 @end
 
 @implementation SPALoginViewController
@@ -20,6 +30,7 @@
     if (self) {
         // Custom initialization
         [self setTitle:@"SmartPecker"];
+        
     }
     return self;
 }
@@ -28,6 +39,19 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    //Check if the device has 3.5 inches screen size
+    if([[UIScreen mainScreen] bounds].size.height == 480.0f){
+        self.logoVerticalSpaceConstraint.constant -= 20.0f;
+        self.loginTextFieldVerticalSpaceConstraint.constant -= 25.0f;
+        self.passwordTextFieldVerticalSpaceConstraint.constant -= 30.0f;
+        self.forgetPasswordButtonVerticalSpaceConstraint.constant -= 30.0f;
+        self.loginButtonVerticalSpaceConstraint.constant -= 30.0f;
+    }
+   
+    [self.loginTextField setKeyboardType:UIKeyboardTypeEmailAddress];
+    [self.passwordTextField setKeyboardType:UIKeyboardTypeDefault];
+    [self.loginTextField becomeFirstResponder];
+    
 }
 
 - (void)didReceiveMemoryWarning
